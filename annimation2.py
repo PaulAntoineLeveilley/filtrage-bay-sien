f03a704cef35fda1d36a2eea391a978ddbeeae51
pa.leveilley <pa.leveilley@groupeonepoint.com>
2022-10-28 16:59:30 +0200

    animation

--- annimation2.py
+++ annimation2.py
@@ -16,16 +16,25 @@ h_mnt = np.flip(h_mnt,axis = 0)
 x_mnt = data["x_MNT"]
 y_mnt = data["y_MNT"]
 
-
 fig = plt.figure()
+plt.title('Trajectoire et estimation par filtrage particulaire au cours du temps')
 
-im = plt.imshow(h_mnt,extent = [125000,225000,125000,225000], animated=True)
+y_mnt_1 = np.flip(y_mnt[0])
+
+indice_y_min=350
+indice_y_max=550
+indice_x_min=400
+indice_x_max=600
+
+im = plt.imshow(h_mnt[indice_y_min:indice_y_max,indice_x_min:indice_x_max], extent = [x_mnt[0,indice_x_min],x_mnt[0,indice_x_max],y_mnt_1[indice_y_max],y_mnt_1[indice_y_min]], animated = True)
+# im = plt.imshow(h_mnt,extent = [x_mnt[0,0],x_mnt[0,-1],y_mnt[0,0],y_mnt[0,-1]], animated=True)
 ax = plt.gca()
 line, = ax.plot([], [],'r') 
 line1, = ax.plot([], [],'g') 
 
 def updatefig(i):
-    im.set_array(h_mnt)
+    h = h_mnt[indice_y_min:indice_y_max,indice_x_min:indice_x_max]
+    im.set_array(h)
     line.set_data(particules[i,:,0],particules[i,:,1])
     line1.set_data(trajectoire[:i,0],trajectoire[:i,1])
     return im,line,line1
